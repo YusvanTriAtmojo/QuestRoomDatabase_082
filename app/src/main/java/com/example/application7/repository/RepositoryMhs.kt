2a12f36f3acdd585265a35d384d1c4ca0a707ba4
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-14 17:47:02 +0700

    Menambahkan fungsi getAllMhs, getMhs, deleteMhs, dan updateMhs untuk memanggil fungsi dari yang telah dibuat di MahasiswaDao untuk mengelola data mahasiswa

--- app/src/main/java/com/example/application7/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/application7/repository/RepositoryMhs.kt
@@ -1,7 +1,16 @@
 package com.example.application7.repository
 
 import com.example.application7.data.entity.Mahasiswa
+import java.util.concurrent.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
+    fun getMhs(nim: String): Flow<Mahasiswa>
+
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 }
