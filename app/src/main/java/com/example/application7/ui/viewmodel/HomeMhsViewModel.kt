b9f7ce8e1f7bc9f87999adfb374857e0215ea128
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-14 21:35:51 +0700

    catch untuk menangkap dan menampilkan error saat loading, stateIn untuk mengelola state UI agar selalu dipantau atau diperbarui

--- app/src/main/java/com/example/application7/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/application7/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,13 +1,17 @@
 package com.example.application7.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.application7.data.entity.Mahasiswa
 import com.example.application7.repository.RepositoryMhs
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class HomeMhsViewModel(
     private val repositoryMhs: RepositoryMhs
@@ -24,7 +28,22 @@ class HomeMhsViewModel(
             emit(HomeUiState(isLoading = true))
             delay(900)
         }
-
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
 }
 
 data class HomeUiState(
