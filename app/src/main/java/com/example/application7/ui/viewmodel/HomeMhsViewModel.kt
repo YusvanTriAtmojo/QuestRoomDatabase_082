d65c054663f2a07111811d71ea2a1e9d3d030982
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-14 21:29:52 +0700

    filterNotNull untuk mencegah memuat data NULL, map untuk memetakan data menjadi list, onStart untuk memuat data awal atau menampilkan proses loading

--- app/src/main/java/com/example/application7/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/application7/ui/viewmodel/HomeMhsViewModel.kt
@@ -3,12 +3,28 @@ package com.example.application7.ui.viewmodel
 import androidx.lifecycle.ViewModel
 import com.example.application7.data.entity.Mahasiswa
 import com.example.application7.repository.RepositoryMhs
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 
 class HomeMhsViewModel(
     private val repositoryMhs: RepositoryMhs
 ) : ViewModel() {
     val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+
 }
 
 data class HomeUiState(
