2a49136664d13c7a879c08f95de75b4b6d273f60
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-14 21:10:50 +0700

    Membuat Class HomeMhsViewModel. Membuat variabel repositoryMhs yang terhubung dengan file RepositoryMhs. Lalu, variabel homeUiState untuk memantau keadaan perubahan data pada proses getAllMhs

--- app/src/main/java/com/example/application7/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/application7/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,7 +1,15 @@
 package com.example.application7.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
 import com.example.application7.data.entity.Mahasiswa
+import com.example.application7.repository.RepositoryMhs
+import kotlinx.coroutines.flow.StateFlow
 
+class HomeMhsViewModel(
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+    val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+}
 
 data class HomeUiState(
     val listMhs: List<Mahasiswa> = listOf(),
