9e965775f253c3103d9d867abe9517cfca59ff51
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-15 16:01:14 +0700

    Membuat data class DetailUiState yang berisi variabel-variabel untuk menampung data yang akan ditampilkan di UI yang akan digunakan di class DetailMhsViewModel

---
+++ app/src/main/java/com/example/application7/ui/viewmodel/DetailMhsViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.application7.ui.viewmodel
+
+
+
+class DetailMhsViewModel(
+) {
+
+}
+
+
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+
+/* Data class untuk menampung data yang akan ditampilkan di UI */
