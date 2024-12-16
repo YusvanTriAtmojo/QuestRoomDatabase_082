94b6fbd15de3988b0a4840e57ed1c230cf7ecea7
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-16 16:18:07 +0700

    Di fungsi pengelolaHalaman ditambahkan navigasi DestinasiHome dan DestinasiInsert dengan menambahkan fungsi Viewnya dengan parameternya, dan DestinasiHome sebagai destinasi awal

--- app/src/main/java/com/example/application7/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/application7/ui/navigation/PengelolaHalaman.kt
@@ -7,6 +7,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.application7.ui.view.mahasiswa.DestinasiInsert
+import com.example.application7.ui.view.mahasiswa.HomeMhsView
 import com.example.application7.ui.view.mahasiswa.InsertMhsView
 
 @Composable
@@ -16,14 +17,37 @@ fun PengelolaHalaman(
 ) {
     NavHost(
         navController = navController,
-        startDestination = DestinasiInsert.route) {
+        startDestination = DestinasiHome.route) {
+
+        composable(
+            route = DestinasiHome.route
+        ) {
+            HomeMhsView(
+                onDetailClick = {nim ->
+                    navController.navigate("${DestinasiDetail.route}/$nim")
+                    println(
+                        "PengelolaHalaman: nim = $nim"
+                    )
+                },
+                onAddMhs = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+                modifier = modifier
+            )
+        }
+
         composable(
             route = DestinasiInsert.route
         ) {
             InsertMhsView(
-                onBack = {}, onNavigate = { })
-
+                onBack = {
+                    navController.popBackStack()
+                },
+                onNavigate = {
+                    navController.popBackStack()
+                },
+                modifier = modifier,
+                )
         }
-
     }
 }
