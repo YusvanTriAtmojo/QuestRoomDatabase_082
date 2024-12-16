0e0a042933fcee5f395f26ccf63a1499dc4ae56e
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-16 13:07:14 +0700

    Di Class UpdateMhsViewModel variabel repositoryMhs untuk terhubung dengan RepositoryMhs, lalu variabel updateUIState untuk mengelola UI state, variabel _nim dengan savedStateHandle untuk mengambil data nim dari navigasi DestinasiUpdate

--- app/src/main/java/com/example/application7/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/application7/ui/viewmodel/UpdateMhsViewModel.kt
@@ -1,11 +1,24 @@
 package com.example.application7.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.application7.data.entity.Mahasiswa
+import com.example.application7.repository.RepositoryMhs
+import com.example.application7.ui.navigation.DestinasiUpdate
 
 
 class UpdateMhsViewModel (
-)  {
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+    var updateUIState by mutableStateOf(MhsUIState())
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
 }
+
 
 fun Mahasiswa.toUIStateMhs() : MhsUIState = MhsUIState(
     mahasiswaEvent = this.toDetailUiEvent(),
