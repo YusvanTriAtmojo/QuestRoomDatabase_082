51be5ccdeb43ec4a4b6d1610d27de441490d396b
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-16 15:40:12 +0700

    Membuat fungsi UpdateMhsView untuk tampilan update dengan viewModel UpdateMhsViewModel dengan factory PenyediaViewModel. Lalu, mengambil updateUIState dari viewmodel, kemudian mengobservasi perubahan snackbarMessage. Lalu membuat Scaffold yang berisi snackbarHost dan topBar

---
+++ app/src/main/java/com/example/application7/ui/view/mahasiswa/UpdateMhsView.kt
@@ -0,0 +1,62 @@
+package com.example.application7.ui.view.mahasiswa
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.application7.ui.customwidget.TopAppBar
+import com.example.application7.ui.viewmodel.PenyediaViewModel
+import com.example.application7.ui.viewmodel.UpdateMhsViewModel
+import kotlinx.coroutines.launch
+
+@Composable
+fun UpdateMhsView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateMhsViewModel = viewModel(factory = PenyediaViewModel.Factory) //Inisialisasi ViewModel
+
+) {
+    val uiState = viewModel.updateUIState //Ambil UI state dari ViewModel
+    val snackbarHostState = remember { SnackbarHostState() } //Snacbar State
+    val coroutineScope = rememberCoroutineScope()
+
+    //Observasi perubahan snackbarMessage
+    LaunchedEffect(uiState.snackBarMessage) {
+        println("LaunchedEffect triggered")
+        uiState.snackBarMessage?.let {message ->
+            println("Snackbar message received: $message")
+            coroutineScope.launch {
+                println("Launching coroutine for snackbar")
+                snackbarHostState.showSnackbar(
+                    message = message,
+                    duration = SnackbarDuration.Long
+                )
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+
+    Scaffold(
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }, //Tempatkan snackbar di Scaffold
+        topBar = {
+            TopAppBar(
+                judul = "Edit Mahasiswa",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+            )
+        }
+    )
+    {
+
+    }
+
+}
