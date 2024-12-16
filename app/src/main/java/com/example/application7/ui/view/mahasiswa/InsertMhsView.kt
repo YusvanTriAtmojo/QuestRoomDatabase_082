3f1fe5fe6e8279a6309f4a2db2078a97ca89c45e
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-16 16:41:44 +0700

    Menambahkan modifier di TopAppBar dan InsertMhsView

--- app/src/main/java/com/example/application7/ui/view/mahasiswa/InsertMhsView.kt
+++ app/src/main/java/com/example/application7/ui/view/mahasiswa/InsertMhsView.kt
@@ -75,7 +75,8 @@ fun InsertMhsView(
             TopAppBar(
                 onBack = onBack,
                 showBackButton = true,
-                judul = "Tambah Mahasiswa"
+                judul = "Tambah Mahasiswa",
+                modifier = modifier
             )
 
             //Isi Body
